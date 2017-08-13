fbd4f7fc5fe73a9c421eca15e90b693f528ffebe
Mudathir Lawal <al-mudathir@live.com>
2017-08-13 03:01:12 +0100

    Final & Tested Version of TaxBill Calculator Prog.
    13th August, 2017 - Mudathir Lawal

--- TaxPayer.php
+++ TaxPayer.php
@@ -1,44 +1,43 @@
 <?php
 class TaxPayer 
 {
-    public $payersFirstName;
-    public $payersLastName;
-    public $totalTaxBill;
-    public $annualIncome;
-    public $firstRemainder;
-    public $secondRemainder;
-    public $thirdRemainder;
-    public $fourthRemainder;
-    public $fifthRemainder;
-    public $finalRemainder;
-    public $initialTaxCharge1;
-    public $initialTaxCharge2;
-    public $initialTaxCharge3;
-    public $initialTaxCharge4;
-    public $initialTaxCharge5;
-    public $initialTaxCharge6;
+    public  $firstName;
+    public  $lastName;
+    public  $totalTaxBill;
+    public  $annualIncome;
+    private $firstRemainder;
+    private $secondRemainder;
+    private $thirdRemainder;
+    private $fourthRemainder;
+    private $fifthRemainder;
+    private $initialTaxCharge1;
+    private $initialTaxCharge2;
+    private $initialTaxCharge3;
+    private $initialTaxCharge4;
+    private $initialTaxCharge5;
+    private $initialTaxCharge6;
     
-    const FIRST_TAX_RATE    = 0.00;
-    const SECOND_TAX_RATE   = 0.10;
-    const THIRD_TAX_RATE    = 0.15;
-    const FOURTH_TAX_RATE   = 0.20;
-    const FIFTH_TAX_RATE    = 0.25;
-    const SIXTH_TAX_RATE    = 0.30;  
-    const INCOME_PORTION_1  = 1000.00;  
-    const INCOME_PORTION_2  = 9000.00;    
-    const INCOME_PORTION_3  = 10200.00; 
-    const INCOME_PORTION_4  = 10550.00; 
-    const INCOME_PORTION_5  = 19250.00; 
+    protected const FIRST_TAX_RATE    = 0.00;
+    protected const SECOND_TAX_RATE   = 0.10;
+    protected const THIRD_TAX_RATE    = 0.15;
+    protected const FOURTH_TAX_RATE   = 0.20;
+    protected const FIFTH_TAX_RATE    = 0.25;
+    protected const SIXTH_TAX_RATE    = 0.30;  
+    protected const INCOME_PORTION_1  = 1000.00;  
+    protected const INCOME_PORTION_2  = 9000.00;    
+    protected const INCOME_PORTION_3  = 10200.00; 
+    protected const INCOME_PORTION_4  = 10550.00; 
+    protected const INCOME_PORTION_5  = 19250.00; 
     
-    public function __construct($payersLastName, $payersFirstName, $annualIncome) {
-        $this->payersLastName  = $payersLastName;
-        $this->payersFirstName = $payersFirstName;
-        $this->annualIncome    = $annualIncome;
+    public function __construct($lastName, $firstName, $annualIncome) {
+        $this->lastName     = $lastName;
+        $this->firstName    = $firstName;
+        $this->annualIncome = $annualIncome;  
     }
     
     public function __toString() {
-        return $this->payersLastName . ", " . $this->payersFirstName . ":  " . 
-               $this->getAnnualTaxBill($annualIncome) . PHP_EOL;
+        return $this->lastName . ", " . $this->firstName . ":  " . 
+               $this->getAnnualTaxBill($this->annualIncome) . PHP_EOL;
     }
     
     public function getAnnualIncome() {
@@ -49,82 +48,81 @@ class TaxPayer
         $this->annualIncome = $annualIncome;
     }
     
-    public function editPayersName($payersLastName, $payersFirstName) {
-        $this->payersLastname  = $payersLastName;
-        $this->payersFirstName = $payersFirstName;
+    public function editPayersName($lastName, $firstName) {
+        $this->lastname  = $lastName;
+        $this->firstName = $firstName;
     }
     
     public function getPayersName() {
-        return "NAME: " . $payersLastName . " " . $payersFirstName . PHP_EOL;
+        return "NAME: " . $this->lastName . " " . $this->firstName . PHP_EOL;
     }
     
     public function getAnnualTaxBill($annualIncome) {
-    if( $annualIncome <= INCOME_PORTION_1 ) {
-        $initialTaxCharge1 = $annualIncome * FIRST_TAX_RATE;
-        return $totalTaxBill = $initialTaxCharge1;
+    if( $this->annualIncome <= self::INCOME_PORTION_1 ) {
+        $this->initialTaxCharge1 = $this->annualIncome * self::FIRST_TAX_RATE;
+        return $this->totalTaxBill = $this->initialTaxCharge1;
     }
     else {
-        $firstRemainder = $annualIncome - INCOME_PORTION_1;
-        if( $firstRemainder <= INCOME_PORTION_2 ) {
-            $initialTaxCharge2 = $firstRemainder * SECOND_TAX_RATE;
-            $initialTaxCharge1 = $annualIncome * FIRST_TAX_RATE;
-            return $totalTaxBill = $initialTaxCharge1 + $initialTaxCharge2;
+        $this->firstRemainder = $this->annualIncome - self::INCOME_PORTION_1;
+        if( $this->firstRemainder   <= self::INCOME_PORTION_2 ) {
+            $this->initialTaxCharge2 = $this->firstRemainder  * self::SECOND_TAX_RATE;
+            $this->initialTaxCharge1 = self::INCOME_PORTION_1 * self::FIRST_TAX_RATE;
+            return $this->totalTaxBill = $this->initialTaxCharge1 + $this->initialTaxCharge2;
         } 
         else {
-            $secondRemainder = $firstRemainder - INCOME_PORTION_2;
-            if( $secondRemainder <= INCOME_PORTION_3 ) {
-                $initialTaxCharge3 = $secondRemainder * THIRD_TAX_RATE;
-                $initialTaxCharge2 = $firstRemainder * SECOND_TAX_RATE;
-                $initialTaxCharge1 = $annualIncome * FIRST_TAX_RATE;
-                return $totalTaxBill =   
-                       $initialTaxCharge1 + $initialTaxCharge2 + $initialTaxCharge3;
+            $this->secondRemainder = $this->firstRemainder - self::INCOME_PORTION_2;
+            if( $this->secondRemainder    <= self::INCOME_PORTION_3 ) {
+                $this->initialTaxCharge3   = $this->secondRemainder * self::THIRD_TAX_RATE;
+                $this->initialTaxCharge2   = self::INCOME_PORTION_2 * self::SECOND_TAX_RATE;
+                $this->initialTaxCharge1   = self::INCOME_PORTION_1 * self::FIRST_TAX_RATE;
+                return $this->totalTaxBill =   
+                       $this->initialTaxCharge1 + $this->initialTaxCharge2 + $this->initialTaxCharge3;
             }
             else {
-                $thirdRemainder = $secondRemainder - INCOME_PORTION_3;
-                if( $thirdRemainder <= INCOME_PORTION_4 ) {
-                    $initialTaxCharge4 = $thirdremainder * FOURTH_TAX_RATE;
-                    $initialTaxCharge3 = $secondRemainder * THIRD_TAX_RATE;
-                    $initialTaxCharge2 = $firstRemainder * SECOND_TAX_RATE;
-                    $initialTaxCharge1 = $annualIncome * FIRST_TAX_RATE;
-                    return $totalTaxBill = $initialTaxCharge1 + $initialTaxCharge2 + 
-                                           $initialTaxCharge3 + $initialTaxCharge4;
+                $this->thirdRemainder = $this->secondRemainder - self::INCOME_PORTION_3;
+                if( $this->thirdRemainder   <= self::INCOME_PORTION_4 ) {
+                    $this->initialTaxCharge4 = $this->thirdRemainder  * self::FOURTH_TAX_RATE;
+                    $this->initialTaxCharge3 = self::INCOME_PORTION_3 * self::THIRD_TAX_RATE;
+                    $this->initialTaxCharge2 = self::INCOME_PORTION_2 * self::SECOND_TAX_RATE;
+                    $this->initialTaxCharge1 = self::INCOME_PORTION_1 * self::FIRST_TAX_RATE;
+                    return $this->totalTaxBill = $this->initialTaxCharge1 + $this->initialTaxCharge2 + 
+                                           $this->initialTaxCharge3 + $this->initialTaxCharge4;
                 } 
                 else {
-                    $fourthRemainder = $thirdRemainder - INCOME_PORTION_4;
-                    if( $fourthRemainder <= INCOME_PORTION_5 ) {
-                        $initialTaxCharge5 = $fourthRemainder * FIFTH_TAX_RATE;
-                        $initialTaxCharge4 = $thirdremainder * FOURTH_TAX_RATE;
-                        $initialTaxCharge3 = $secondRemainder * THIRD_TAX_RATE;
-                        $initialTaxCharge2 = $firstRemainder * SECOND_TAX_RATE;
-                        $initialTaxCharge1 = $annualIncome * FIRST_TAX_RATE;
-                        return $totalTaxBill = $initialTaxCharge1 + $initialTaxCharge2 + 
-                               $initialTaxCharge3 + $initialTaxCharge4 + $initialTaxCharge5;
+                    $this->fourthRemainder = $this->thirdRemainder - self::INCOME_PORTION_4;
+                    if( $this->fourthRemainder  <= self::INCOME_PORTION_5 ) {
+                        $this->initialTaxCharge5 = $this->fourthRemainder * self::FIFTH_TAX_RATE;
+                        $this->initialTaxCharge4 = self::INCOME_PORTION_4 * self::FOURTH_TAX_RATE;
+                        $this->initialTaxCharge3 = self::INCOME_PORTION_3 * self::THIRD_TAX_RATE;
+                        $this->initialTaxCharge2 = self::INCOME_PORTION_2 * self::SECOND_TAX_RATE;
+                        $this->initialTaxCharge1 = self::INCOME_PORTION_1 * self::FIRST_TAX_RATE;
+                        return $this->totalTaxBill = $this->initialTaxCharge1 + $this->initialTaxCharge2 + 
+                               $this->initialTaxCharge3 + $this->initialTaxCharge4 + $this->initialTaxCharge5;
                     }  
                     else { 
-                          $finalRemainder = $annualIncome - (INCOME_PORTION_1 + INCOME_PORTION_2 +                                                       INCOME_PORTION_3 + INCOME_PORTION_4 + INCOME_PORTION_5 );
-                          $initialTaxCharge6 = $finalRemainder * SIXTH_TAX_RATE;
-                          $initialTaxCharge5 = $fourthRemainder * FIFTH_TAX_RATE;
-                          $initialTaxCharge4 = $thirdremainder * FOURTH_TAX_RATE;
-                          $initialTaxCharge3 = $secondRemainder * THIRD_TAX_RATE;
-                          $initialTaxCharge2 = $firstRemainder * SECOND_TAX_RATE;
-                          $initialTaxCharge1 = $annualIncome * FIRST_TAX_RATE;
-                          return $totalTaxBill = $initialTaxCharge1 + $initialTaxCharge2 + 
-                                 $initialTaxCharge3 + $initialTaxCharge4 + $initialTaxCharge5 + 
-                                 $initialTaxCharge6;
+                          $this->fifthRemainder    = $this->annualIncome - (self::INCOME_PORTION_1 +                                         self::INCOME_PORTION_2 + self::INCOME_PORTION_3 + self::INCOME_PORTION_4                                       + self::INCOME_PORTION_5 );
+                          $this->initialTaxCharge6 = $this->fifthRemainder  * self::SIXTH_TAX_RATE;
+                          $this->initialTaxCharge5 = self::INCOME_PORTION_5 * self::FIFTH_TAX_RATE;
+                          $this->initialTaxCharge4 = self::INCOME_PORTION_4 * self::FOURTH_TAX_RATE;
+                          $this->initialTaxCharge3 = self::INCOME_PORTION_3 * self::THIRD_TAX_RATE;
+                          $this->initialTaxCharge2 = self::INCOME_PORTION_2 * self::SECOND_TAX_RATE;
+                          $this->initialTaxCharge1 = self::INCOME_PORTION_1 * self::FIRST_TAX_RATE;
+                          return $this->totalTaxBill = $this->initialTaxCharge1 + $this->initialTaxCharge2 + 
+                                 $this->initialTaxCharge3 + $this->initialTaxCharge4 + $this->initialTaxCharge5                                  + $this->initialTaxCharge6 . PHP_EOL;
                     }
                 }
             } 
         }
     }
     }
-}   # End of class definition.
-#........................................................................................................
+} # End of class definition.
+#...........................................................................................................................
 
 
-
-$taxPayer1 = new TaxPayer("BULL", "John", 97000.00);
-echo $taxPayer1->getAnnualTaxBill(97000.00);
-//echo $taxPayer1;
+// Method calls:
+$taxPayer1 = new TaxPayer("AMBODE", "Akinwumi", 50000.00);
+echo $taxPayer1;
+//echo $taxPayer1->getAnnualTaxBill(12000000.00);
 //var_dump($taxPayer1);
 
 
